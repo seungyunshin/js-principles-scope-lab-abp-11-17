12a251dbdf19830cf33b1e75ef18d2f62c1d9af7
Seung-Yun <sseungyun@gmail.com>
2017-11-14 03:14:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ function setBestCustomer(){
 function overwriteBestCustomer(){
   bestCustomer = 'maybe bob'
 }
+
+const leastFavoriteCustomer = 'benson'
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'brendan'
+}
